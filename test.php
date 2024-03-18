f9b5f5a3294dc583946c2d4146aa8d2ca35f3272
diegogj17 <diegogj17@hotmail.es>
2024-03-18 12:50:24 +0100

    funcion añadir array de ids para comparacion

--- test.php
+++ test.php
@@ -1,43 +1,48 @@
 <?php
-    include("pablo.php");
+include ("pablo.php");
 
-    $sql = "SELECT * FROM solutia";
-    $result = mysqli_query($conn,$sql);
+$sql = "SELECT * FROM solutia";
+$result = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)){
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
         echo $row["Id"] . "<br>";
         echo $row["Fecha"] . "<br>";
         echo $row["Asunto"] . "<br>";
         echo $row["Mensaje"] . "<br>";
         echo $row["Estado"] . "<br>";
-        };
-
-    }else{
-        echo "Ningun usuario";
     }
+    ;
 
-    mysqli_close($conn);
+} else {
+    echo "Ningun usuario";
+}
+
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <?php 
-    
-    $array = array('305890','123630');
-    do{
-    $mostrar = crearRandom();
-    }while(in_array($mostrar, $array));
-    echo"".$mostrar."";
-    
 
-    function crearRandom(){
+<body>
+    <?php
+
+    $texto = arrayTXT();
+    do {
+        $mostrar = crearRandom();
+
+    } while (in_array($mostrar, $texto));
+    echo "" . $mostrar . "";
+
+
+    function crearRandom()
+    {
         $guardar = "";
         for ($i = 0; $i < 6; $i++) {
             $numero = rand(0, 9);
@@ -45,11 +50,31 @@
         }
         return $guardar;
     }
-  
-        
+
+    function arrayTXT()
+    {
+        echo "Has entrado en la funcion de arrayTXT" . "<br>";
+        include ("pablo.php");
+        $ideses = array();
+        $tabla = "SELECT `Id` FROM solutia";
+        $resultado = mysqli_query($conn, $tabla);
+        $contador = 0;
+        while ($row = mysqli_fetch_assoc($resultado)) {
+            // echo $row["Id"] . "<br>";
     
-    
+            $ideses[$contador] = $row["Id"];
+            $contador++;
+
+        }
+        ;
+        mysqli_close($conn);
+        return $ideses;
+    }
+
+
+
 
     ?>
-    </body>
+</body>
+
 </html>
