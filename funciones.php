12ef923afa9872fd67edd66495cce906dedb9b9c
diegogj17 <diegogj17@hotmail.es>
2024-03-19 14:08:13 +0100

    no me acuerdo lo que he hecho la vd

--- funciones.php
+++ funciones.php
@@ -1,90 +1,90 @@
-
-
 <?php
-function insertar($Asunto, $Mensaje){
-    include("pablo.php");
-    
+function insertar($Asunto, $Mensaje)
+{
+    include ("pablo.php");
+
     $id = crearRandom();
     $Asunto = mysqli_real_escape_string($conn, $Asunto);
     $Mensaje = mysqli_real_escape_string($conn, $Mensaje);
-    
+
     $sql = "INSERT INTO solutia (Id,  Fecha, Asunto, Mensaje)
                         values ('$id', NOW(), '$Asunto', '$Mensaje')";
 
-    mysqli_query($conn,$sql);
+    mysqli_query($conn, $sql);
 
     //Poner email del abogado a la que se le envia la denuncia
     $para = '';
 
-    
-    mail($para,$Asunto, $Mensaje);
 
-    
+    mail($para, $Asunto, $Mensaje);
+
+
     mysqli_close($conn);
     return $id;
 }
-    function crearRandom()
-    {
-        $arrayID = addIDtoArray();
-        do{
-            $guardar = "";
-            for ($i = 0; $i < 6; $i++) {
-                $numero = rand(0, 9);
-                $guardar .= $numero;
-            }
-            }while(in_array($guardar, $arrayID));
-        return $guardar;
-    }
+function insertarConID($id, $Asunto, $Mensaje)
+{
+    include ("pablo.php");
+    $Asunto = mysqli_real_escape_string($conn, $Asunto);
+    $Mensaje = mysqli_real_escape_string($conn, $Mensaje);
 
-    function addIDtoArray()
-    {
-        include ("pablo.php");
-        $ideses = array();
-        $tabla = "SELECT `Id` FROM solutia";
-        $resultado = mysqli_query($conn, $tabla);
-        $contador = 0;
-        while ($row = mysqli_fetch_assoc($resultado)) {    
-            $ideses[$contador] = $row["Id"];
-            $contador++;
-        }
-        ;
-        mysqli_close($conn);
-        return $ideses;
-    }
-    function sacarTexto($id){
-        include("pablo.php");
-        $definitivo = "";
-        $variable = "SELECT `Asunto`, `Mensaje`  FROM solutia WHERE Id = $id";
-        $result = mysqli_query($conn,$variable);
-        if(mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $definitivo.=$row["Asunto"] . $row["Mensaje"];
-        }}else{
-            echo"No existe ese id maquina";
-        }
-        mysqli_close($conn);
-        return $definitivo;
-    }
-    ?>
+    $sql = "INSERT INTO solutia (Id,  Fecha, Asunto, Mensaje)
+                        values ('$id', NOW(), '$Asunto', '$Mensaje')";
 
-<!-- <?php
-    include("pablo.php");
+    mysqli_query($conn, $sql);
 
-    $sql = "SELECT * FROM solutia";
-    $result = mysqli_query($conn,$sql);
+    //Poner email del abogado a la que se le envia la denuncia
+    $para = '';
 
-    if(mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)){
-        echo $row["Id"] . "<br>";
-        echo $row["Fecha"] . "<br>";
-        echo $row["Asunto"] . "<br>";
-        echo $row["Mensaje"] . "<br>";
-        echo "<br>";
-        };
 
-    }else{
-        echo "Ningun usuario se ha insertado";
-    }
+    mail($para, $Asunto, $Mensaje);
+
 
     mysqli_close($conn);
-?> -->
+    return $id;
+}
+function crearRandom()
+{
+    $arrayID = addIDtoArray();
+    do {
+        $guardar = "";
+        for ($i = 0; $i < 6; $i++) {
+            $numero = rand(0, 9);
+            $guardar .= $numero;
+        }
+    } while (in_array($guardar, $arrayID));
+    return $guardar;
+}
+
+function addIDtoArray()
+{
+    include ("pablo.php");
+    $ideses = array();
+    $tabla = "SELECT `Id` FROM solutia";
+    $resultado = mysqli_query($conn, $tabla);
+    $contador = 0;
+    while ($row = mysqli_fetch_assoc($resultado)) {
+        $ideses[$contador] = $row["Id"];
+        $contador++;
+    }
+    ;
+    mysqli_close($conn);
+    return $ideses;
+}
+function sacarTexto($id)
+{
+    include ("pablo.php");
+    $definitivo = "";
+    $variable = "SELECT `Asunto`, `Mensaje`  FROM solutia WHERE Id = $id";
+    $result = mysqli_query($conn, $variable);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $definitivo .= "Asunto: " . $row["Asunto"] . "<br>" . "Mensaje: " . $row["Mensaje"];
+        }
+    } else {
+        echo "No existe ese id";
+    }
+    mysqli_close($conn);
+    return $definitivo;
+}
+?>
